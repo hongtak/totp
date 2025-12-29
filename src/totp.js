711119f63036983e9558858cf667be3f52e9058c
Choi Hong Tak <hongtak@gmail.com>
2025-12-29 14:49:54 +0800

    Removed unnecessary console.log

--- src/totp.js
+++ src/totp.js
@@ -9,7 +9,6 @@ function generate (secret, opts = {}) {
   }
 
   Object.assign(defaultOpts, opts)
-  console.log(defaultOpts)
   const counter = Math.floor(defaultOpts.epoch / (defaultOpts.step * 1000))
   return hotp.generate(secret, counter, defaultOpts)
 }
@@ -24,11 +23,9 @@ function verify (secret, token, opts = {}) {
   }
 
   Object.assign(defaultOpts, opts)
-  console.log(defaultOpts)
   const counter = Math.floor(defaultOpts.epoch / (defaultOpts.step * 1000))
 
   for (let i = -defaultOpts.window; i <= defaultOpts.window; i++) {
-    console.log(counter + i)
     const expectedCode = hotp.generate(secret, counter + i, defaultOpts)
     if (expectedCode === token) {
       return true
